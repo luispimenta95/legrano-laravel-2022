35d70f4aeafcc800902508b4446c396695f0e812
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2021-12-27 12:08:21 -0300

    Reajustando mapeamentos

--- database/migrations/2021_12_18_100906_create_cidades_table.php
+++ database/migrations/2021_12_18_100906_create_cidades_table.php
@@ -16,7 +16,7 @@ class CreateCidadesTable extends Migration
         Schema::create('cidades', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nome', 50);
-            $table->float('precoFrete', 8, 2);
+            $table->float('precoFrete', 8, 2)->default(0);
             $table->boolean('entrega')->default(0);
             $table->integer('estado')->unsigned();
             $table->foreign('estado')->references('id')->on('estados');
